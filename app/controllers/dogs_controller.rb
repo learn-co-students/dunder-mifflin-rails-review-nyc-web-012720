d87bbe0a6e7b6f5be9ecbe696ea9bd4e84149e6e
davidmlee4 <davidmlee4@gmail.com>
2020-02-26 15:38:15 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,48 @@
 class DogsController < ApplicationController
+    def index
+        @dogs = Dog.all.sort {|b,a|a.employees.count <=> b.employees.count}
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+        if @dog.valid?
+            redirect_to dog_path(@dog)
+        else
+            flash[:errors] = @dog.errors.full_messages
+            redirect_to new_dog_path
+        end
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+    end
+
+    def update
+        @dog = Dog.find(params[:id])
+        if @dog.update(dog_params)
+            redirect_to dog_path(@dog)
+        else
+            flash[:errors] = @dog.errors.full_messages
+            redirect_to edit_dog_path
+        end
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect_to dogs_path 
+    end
+
+    private 
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
 end
