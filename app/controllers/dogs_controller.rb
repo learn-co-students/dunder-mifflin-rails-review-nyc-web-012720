dd466ac6d1758bedd3da0c7573c0ada5fd11eff1
aprilfaithdays <aprilfaith.e@gmail.com>
2020-02-26 18:57:35 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,10 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all.sort{|a,b| b.employees.count <=> a.employees.count}
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
 end
